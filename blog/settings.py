53d0e83edf5519351bae3b350a6c34064aba0bb0
Tuss4 <tuss4dbfn@gmail.com>
2013-07-15 02:01:35 -0500

    changing the location of staticfile_dirs

--- blog/settings.py
+++ blog/settings.py
@@ -80,7 +80,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'templates/static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

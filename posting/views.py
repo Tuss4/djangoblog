d437dcd1037ea9beae0572c66acdceb42a215bc8
Tuss4 <tuss4dbfn@gmail.com>
2013-07-16 01:18:40 -0500

    nothing has changed. I'll check for some django-disqus packages

--- posting/views.py
+++ posting/views.py
@@ -9,22 +9,8 @@ def main(request):
 
 def post(request, single):
 	single = Post.objects.filter(id=single)
-	disqus_code = '''<div id="disqus_thread"></div>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'tuss4dzigns'; // required: replace example with your forum shortname
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>'''
-    comments = []
-    comments.append(disqus_code)
+	disqus_code = ['<div id="disqus_thread"></div>','<script type="text/javascript">','var disqus_shortname = "tuss4dzigns";','(function() {','var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;','dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";','(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);','})();','</script>','<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>',
+    	'<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>']
 	if not single:
 		raise Http404()
-	return render(request, "post.html", {"post": single, "comments": comments})
+	return render(request, "post.html", {"post": single, "comments": disqus_code})
